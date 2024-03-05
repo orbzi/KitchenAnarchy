656771cf869a6c45b779f91765ba854b6627e937
osama <osamarizwan3@gmail.com>
2024-03-06 03:05:40 +0500

    Plate Counter added
    added plate counter and full burger recipe visuals placed on a plate

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -41,6 +41,21 @@ public class KitchenObject : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
+            plateKitchenObject = this as PlateKitchenObject;
+            return true;
+        }
+        else
+        {
+            plateKitchenObject = null;
+            return false;
+        }
+
+    }
+
     public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParents kitchenObjectParents)
     {
         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
